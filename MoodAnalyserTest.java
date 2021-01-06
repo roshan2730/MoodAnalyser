abcdd046e0edf9bdaa5ead76b40709301af87d16
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-07 00:00:47 +0530

    uc2:Added Exception handling

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+package moodAnalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+    @Test
+    public void testMood_WhenStringContainSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        String isMoodSad = moodAnalyser.analyseMood();
+        Assert.assertEquals("SAD" , isMoodSad);
+    }
+
+    @Test
+    public void testMood_WhenStringContainHappy_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String isMoodHappy = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY" , isMoodHappy);
+    }
+}
