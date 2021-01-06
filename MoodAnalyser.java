7e7204b160b2f485d80dd08199c6eaab7efc2a1c
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-06 23:59:20 +0530

    uc2:Added Exception handling

---
+++ MoodAnalyser.java
@@ -0,0 +1,30 @@
+package moodAnalyser;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class MoodAnalyser {
+
+    private String moodMessage;
+
+    public MoodAnalyser(){
+    }
+
+    public MoodAnalyser(String moodMessage) {
+        this.moodMessage = moodMessage;
+    }
+
+    public String analyseMood() {
+        try {
+            if(moodMessage.contains("Sad")) {
+                return "SAD";
+            }
+            else {
+                return "HAPPY";
+            }
+        }
+        catch (NullPointerException e) {
+            return "HAPPY";
+        }
+
+    }
+}
